68da64380021254fab72ed827c3b634d7817f83b
SandraUsk <120962582+SandraUsk@users.noreply.github.com>
2022-12-19 00:12:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,15 +10,12 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands = ['start'])
 def start_message(message):
-    bot.send_message(message.chat.id,'Привет! Что вы хотите узнать?')
-@bot.message_handler(commands = ['button'])
-def button_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
     item1 = types.KeyboardButton("Погода")
     item2 = types.KeyboardButton("Восход и заход солнца")
     markup.add(item1)
     markup.add(item2)
-    bot.send_message(message.chat.id,'Выберите, что вам надо :)',reply_markup = markup)
+    bot.send_message(message.chat.id, 'Привет! Что вы хотите узнать? :)', reply_markup=markup)
 @bot.message_handler(content_types = 'text')
 def message_reply(message):
     if message.text == "Погода":
