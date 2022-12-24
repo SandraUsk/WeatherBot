ab68639e9ed8b40bdf8b83bfe392691d8f3bec39
SandraUsk <120962582+SandraUsk@users.noreply.github.com>
2022-12-24 06:41:38 +0300

    Update data.py

--- data.py
+++ data.py
@@ -24,8 +24,20 @@ def get_suntime(city_id):
     weatherapi_call = "http://api.openweathermap.org/data/2.5/forecast?" + city_id + "&appid=" + weatherapi_key
     weatherapi_response = urlopen(weatherapi_call).read()
     weather_dict = json.loads(weatherapi_response)
-    sunrise = str(((weather_dict['city']['sunrise'] + weather_dict['city']['timezone']) % (24*60*60)) // 3600) + ":" + str((((weather_dict['city']['sunrise'] + weather_dict['city']['timezone']) % (24*60*60)) // 60) % 60)
-    sunset = str(((weather_dict['city']['sunset'] + weather_dict['city']['timezone']) % (24*60*60)) // 3600) + ":" + str((((weather_dict['city']['sunset'] + weather_dict['city']['timezone']) % (24*60*60)) // 60) % 60)
+    riseh = str(((weather_dict['city']['sunrise'] + weather_dict['city']['timezone']) % (24 * 60 * 60)) // 3600)
+    if len(riseh) == 1:
+        riseh = "0" + riseh
+    risemin = str((((weather_dict['city']['sunrise'] + weather_dict['city']['timezone']) % (24*60*60)) // 60) % 60)
+    if len(risemin) == 1:
+        risemin = "0" + risemin
+    sunrise = riseh + ":" + risemin
+    seth = str(((weather_dict['city']['sunset'] + weather_dict['city']['timezone']) % (24*60*60)) // 3600)
+    if len(seth) == 1:
+        seth = "0" + seth
+    setmin = str((((weather_dict['city']['sunset'] + weather_dict['city']['timezone']) % (24*60*60)) // 60) % 60)
+    if len(setmin) == 1:
+        setmin = "0" + setmin
+    sunset = seth + ":" + setmin
     s = "Восход солнца в " + str(sunrise) + ", заход - в " + str(sunset)
     return s
 
